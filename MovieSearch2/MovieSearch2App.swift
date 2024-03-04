3e8baab364aebe6553998fc428960dcb5fae680c
Ulises Castillo <uac@fb.com>
2024-03-04 09:38:36 -0800

    Initial Commit

---
+++ MovieSearch2/MovieSearch2App.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearch2App.swift
+//  MovieSearch2
+//
+//  Created by Ulises Castillo on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearch2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
